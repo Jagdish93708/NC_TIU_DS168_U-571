b7a204fed4b28b0e1d63830cfffb780ea71c01cb
Jagdish93708 <69030186+Jagdish93708@users.noreply.github.com>
2020-08-03 19:07:44 +0530

    Add files via upload

---
+++ For_Evaluation_Project_DS168_U-571/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+import { FeesComponent } from './fees/fees.component';
+import { ClerkComponent } from './clerk/clerk.component';
+import { AccountantComponent } from './accountant/accountant.component';
+import { ReportComponent } from './report/report.component';
+import { StudentComponent } from './student/student.component';
+import { StudentupComponent } from './studentup/studentup.component';
+import {
+  HttpClientModule,
+  HTTP_INTERCEPTORS,
+  HttpClient
+} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    CreateComponent,
+    UpdateComponent,
+    FeesComponent,
+    ClerkComponent,
+    AccountantComponent,
+    ReportComponent,
+    StudentComponent,
+    StudentupComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
